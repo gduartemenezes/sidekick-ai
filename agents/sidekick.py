21a8d8b51222d09b9f9e4a6a613415f6d7164482
Gustavo Duarte <gduartemenezes@gmail.com>
2025-06-10 14:05:17 -0300

    feat: add class EvaluatorOutput

--- agents/sidekick.py
+++ agents/sidekick.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from typing_extensions import TypedDict
 from typing import Annotated, List, Any, Optional
 from langgraph.graph.message import add_messages
-
+from pydantic import BaseModel, Field
 
 load_dotenv(override=True)
 
@@ -12,3 +12,10 @@ class State(TypedDict):
     feedback_on_work: Optional[str]
     success_criteria_met: bool
     user_input_needed: bool
+
+class EvaluatorOutput(BaseModel):
+    feedback: str = Field(description="Feedback on the assistant's response")
+    success_criteria_met: bool = Field("Whether the success criteria have been met")
+    user_input_needed: bool = Field("True if more input is needed from the user, or clarifications or the assistant is stuck ")
+    
+    
