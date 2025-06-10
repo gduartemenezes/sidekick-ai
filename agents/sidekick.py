98f5dfdfba129c014cc06b1c45654d5e7604f2f0
Gustavo Duarte <gduartemenezes@gmail.com>
2025-06-10 14:09:44 -0300

    feat: create main class with init method

--- agents/sidekick.py
+++ agents/sidekick.py
@@ -2,7 +2,10 @@ from dotenv import load_dotenv
 from typing_extensions import TypedDict
 from typing import Annotated, List, Any, Optional
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
 from pydantic import BaseModel, Field
+import uuid
+
 
 load_dotenv(override=True)
 
@@ -18,4 +21,14 @@ class EvaluatorOutput(BaseModel):
     success_criteria_met: bool = Field("Whether the success criteria have been met")
     user_input_needed: bool = Field("True if more input is needed from the user, or clarifications or the assistant is stuck ")
     
-    
+class Sidekick:
+    def __init__(self):
+        self.worker_llm_with_tools = None
+        self.evaluator_llm_with_tool = None
+        self.tools = None
+        self.llm_with_tools = None
+        self.graph = None
+        self.sidekick_id = str(uuid.uuidv4())
+        self.memory = MemorySaver()
+        self.browser = None
+        self.playwright = None
