506657f54a795522dab29e6f2efa163ca0b2dfce
Gustavo Duarte <gduartemenezes@gmail.com>
2025-06-10 14:31:48 -0300

    feat: add evaluator_worker to Sidekick class

--- agents/sidekick.py
+++ agents/sidekick.py
@@ -81,8 +81,42 @@ class Sidekick:
                 conversation += f"Assistant: {text}\n"
         return conversation
               
+    def evaluator(self, state: State) -> State:
+        last_response = state["messages"][-1].content
         
+        system_message = f"""You are an evaluator that determines if a task has been completed successfully by an Assistant.
+            Assess the Assistant's last response based on the given criteria. Respond with your feedback, and with your decision on whether the success criteria has been met,
+            and whether more input is needed from the user."""
         
+        user_message = f"""
+            You are evaluating a conversation between the User and Assistant.
+            You decide what action to take based on the last response from the Assistant.
+
+            The entire conversation with the assistant, with the user's original request and all replies, is:
+            {self.format_conversation(state['messages'])}
+
+            The success criteria for this assignment is:
+            {state['success_criteria']}
+
+            And the final response from the Assistant that you are evaluating is:
+            {last_response}
+
+            Respond with your feedback, and decide if the success criteria is met by this response.
+            Also, decide if more user input is required, either because the assistant has a question, needs clarification, or seems to be stuck and unable to answer without help.
+
+            The Assistant has access to a tool to write files. If the Assistant says they have written a file, then you can assume they have done so.
+            Overall you should give the Assistant the benefit of the doubt if they say they've done something. But you should reject if you feel that more work should go into this.
+
+            """
+        evaluator_messages = [SystemMessage(content=system_message), HumanMessage(content=user_message)]
+        evaluator_result = self.evaluator_llm_with_tool.invoke(evaluator_messages)
         
+        new_state = {
+            "messages":[{"role": "assistant", "content": f"Evaluator Feedback on this answer: {evaluator_result.feedback}"}],
+            "feedback_on_work": evaluator_result.feedback,
+            "success_criteria_met": evaluator_result.success_criteria_met,
+            "user_input_needed": evaluator_result.user_input_needed,
+        }
         
+        return new_state
         
