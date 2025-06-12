e9f5ff172215311ebff380e644253612bb6f677d
Gustavo Duarte <gduartemenezes@gmail.com>
2025-06-12 13:16:29 -0300

    feat: define method to proccess user requests

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ async def setup():
     sidekick = Sidekick()
     await sidekick.setup()
     return sidekick
+
+async def process_message(sidekick: Sidekick, message, success_criteria, history):
+    results = await sidekick.run_superstep(message, success_criteria, history)
+    return results, sidekick
